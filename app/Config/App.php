c51e16c4e83826d12c9775448ce874d07c86930e
jaganbdr <jagan.mains@gmail.com>
2023-07-04 10:39:00 +0530

    1

--- app/Config/App.php
+++ app/Config/App.php
@@ -17,7 +17,7 @@ class App extends BaseConfig
      *
      *    http://example.com/
      */
-    public string $baseURL = 'https://vedatrix.xyz/public/';
+    public string $baseURL = 'https://vedatrix.xyz/';
 
     /**
      * Allowed Hostnames in the Site URL other than the hostname in the baseURL.
